81ef0b7da0cc20b7d738bbb734717e237c2cde7b
Aidan Burns <aiburns@wpi.edu>
2023-01-30 11:35:43 -0500

    Changed the method signature of parseFile

--- src/main/java/readCSV.java
+++ src/main/java/readCSV.java
@@ -1,8 +1,32 @@
+import com.opencsv.CSVReader;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class readCSV {
-    public static Object[][] parseFile(File file){
+    public static void main(String[] args) {
+        parseFile(new File("src/main/resources/Friends.txt"));
+
+    }
+    public static List<String[]> parseFile(File file){
+        List<String[]> toReturn = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(file))){
+            for (String[] a : reader){
+
+            }
+
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         // Just commenting
-        return new Object[1][1];
+        return toReturn;
     }
 }
