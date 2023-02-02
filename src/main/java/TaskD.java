b689b60c81d703ade4b8d42642dffcd0e2fb5a84
Aidan Burns <aiburns@wpi.edu>
2023-02-02 16:24:44 -0500

    Fixin shit

--- src/main/java/TaskD.java
+++ src/main/java/TaskD.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
@@ -100,6 +101,7 @@ public class TaskD {
         job.setOutputValueClass(IntWritable.class);
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TaskD.NameMap.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, TaskD.FriendMap.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
--- src/test/java/CanadaCanadaCanadaTest.java
+++ src/test/java/CanadaCanadaCanadaTest.java
@@ -21,10 +21,10 @@ public class CanadaCanadaCanadaTest {
     }
     @Test
     public void TaskD() throws Exception{
-        String[] input = new String[2];
+        String[] input = new String[3];
         input[0] = "/home/aidan/codinShit/CS4433Project1WORKING/java/src/main/resources/MyPage.csv";
-        input[0] = "/home/aidan/codinShit/CS4433Project1WORKING/java/src/main/resources/Friends.csv";
-        input[1] = "/home/aidan/codinShit/CS4433Project1WORKING/java/output";
+        input[1] = "/home/aidan/codinShit/CS4433Project1WORKING/java/src/main/resources/Friends.csv";
+        input[2] = "/home/aidan/codinShit/CS4433Project1WORKING/java/output";
 
         TaskD amazing = new TaskD();
         amazing.debug(input);
