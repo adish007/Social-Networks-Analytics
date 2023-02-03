0ace5b6a73835b172b16a777443d672b6f04f255
Aidan Burns <aiburns@wpi.edu>
2023-02-03 16:56:37 -0500

    Fixin shit

--- src/main/java/TaskD.java
+++ src/main/java/TaskD.java
@@ -1,6 +1,5 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -8,104 +7,105 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
-import java.util.Stack;
 
 public class TaskD {
 
     /*
     This class takes the list of friends and then outputs a map where key = userID, 1
      */
-    public class FriendMap extends Mapper<Object, Text, IntWritable, Text>{
-        private final Text one = new Text("a");
-        @Override
-        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            System.out.println(this.getClass().getName());
-
-            String[] vals = value.toString().split(",");
-            context.write(new IntWritable(new Integer(vals[2]).intValue()), one);
-        }
-    }
-
-    public class NameMap extends Mapper<Object, Text, IntWritable, Text>{
-        private final Text one = new Text("1");
+    public class FriendMap extends Mapper<Object, Text, Text, Text>{
+        protected final Text one = new Text("a");
 
         @Override
         protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            System.out.println(this.getClass().getName());
+            System.out.println("Test!");
 
             String[] vals = value.toString().split(",");
-            context.write(new IntWritable(new Integer(vals[0]).parseInt(vals[0])), new Text(vals[1]));
+            context.write(new Text(vals[2]), one);
         }
     }
 
-    public class FriendReduce
-            extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable>{
-        private IntWritable result = new IntWritable();
+    public class NameMap extends Mapper<Object, Text, Text, Text>{
+
         @Override
-        protected void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            System.out.println("Test2!");
+            String[] vals = value.toString().split(",");
+            context.write(new Text(vals[0]), new Text(vals[1]));
+        }
+    }
+
+/*    public class FriendReduce
+            extends Reducer<Text, Text, Text, Text>{
+        private Text result = new Text();
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
             System.out.println(this.getClass().getName());
 
             int sum = 0;
-            for (IntWritable val : values){
-                sum += val.get();
+            for (Text val : values){
+                sum += new Integer(val.toString()).valueOf(val.toString());
             }
-            result.set(sum);
+            result.set(""+sum);
             context.write(key, result);
         }
-    }
+    }*/
 
     public class NameReduce
-            extends Reducer<IntWritable, Text, Text, IntWritable>{
-        private IntWritable result = new IntWritable();
+            extends Reducer<Text, Text, Text, Text>{
+
+        protected final Text one = new Text("a");
 
         @Override
-        protected void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            System.out.println(this.getClass().getName());
-            String name = "";
-            Stack<Text> stack = new Stack<>();
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            Text name = new Text();
+            for (Text val : values){
 
-            for (Text i : values){
-                if (i.toString().length()<2){
-                    stack.push(i);
+                if (val.toString().equals(one.toString())){
+                    sum++;
                 } else {
-                    name = i.toString();
+                    name = new Text(val);
                 }
             }
-            result.set(stack.size());
-            context.write(new Text(name), result);
+            context.write(name, new Text(""+sum));
+
         }
+
+
+
     }
 
     public void debug(String args[]) throws Exception{
+        System.out.println("sanity check");
         Configuration conf = new Configuration();
+
         Job job = Job.getInstance(conf, "TaskD");
         job.setJarByClass(TaskD.class);
-        //Map
-        job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(Text.class);
-        //Job
+        job.setReducerClass(TaskD.NameReduce.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
 
-        //idk about these
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
 
-        //job.setNumReduceTasks(1);
-        job.setCombinerClass(FriendReduce.class);
-        job.setReducerClass(NameReduce.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TaskD.NameMap.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, TaskD.FriendMap.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TaskD.NameMap.class);
+        Path outputPath = new Path(args[2]);
+        FileOutputFormat.setOutputPath(job, outputPath);
+        outputPath.getFileSystem(conf).delete(outputPath, false);
+
+
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws Exception{
+        TaskD a = new TaskD();
+        a.debug(new String[]{"/home/aidan/codinShit/CS4433Project1WORKING/java/MyPage.csv",
+                "/home/aidan/codinShit/CS4433Project1WORKING/java/Friends.csv",
+                "/home/aidan/codinShit/CS4433Project1WORKING/java/output"});
 
     }
 
