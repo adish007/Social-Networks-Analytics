a4fe8800b6d65064248fed46f8117c577d68888b
adish007 <jainadish007@gmail.com>
2023-02-04 10:33:01 -0500

    Starting stuff 2

---
+++ src/main/java/CanadaCanadaCanada.java
@@ -0,0 +1,58 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class CanadaCanadaCanada {
+
+    public static class CanadizerMapper
+            extends Mapper<Object, Text, Text, Text> {
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+
+            String[] vals = value.toString().split(",");
+            if ("CanadaCanadaCanada".equals(vals[2])){
+                context.write(new Text(vals[1]), new Text(vals[4]));
+            }
+
+
+
+        }
+    }
+
+
+
+    public void debug(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "FourA");
+        job.setNumReduceTasks(0);
+        job.setJarByClass(CanadaCanadaCanada.class);
+        job.setMapperClass(CanadizerMapper.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "FourA");
+        job.setNumReduceTasks(0);
+        job.setJarByClass(CanadaCanadaCanada.class);
+        job.setMapperClass(CanadizerMapper.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+}
